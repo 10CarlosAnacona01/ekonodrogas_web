04960713bb426d523934db98b79a720cc7194d97
Carlos Anacona <estebananacona13@gmail.com>
2025-11-08 13:22:18 -0500

    Subir txt

---
+++ EKONODROGAS_BACKEND/ekonodrogas/src/main/java/com/ekonodrogas/ekonodrogas/persistence/VentasEntity.java
@@ -0,0 +1,41 @@
+package com.ekonodrogas.ekonodrogas.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ventas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VentaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venta")
+    private Long idVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private UserEntity usuario;
+
+    @Column(name = "fecha_venta")
+    private java.time.LocalDateTime fechaVenta;
+
+    @Column(name = "total_venta")
+    private Integer totalVenta;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado_venta")
+    private EstadoVenta estadoVenta;
+
+    public enum EstadoVenta {
+        completada, cancelada, pendiente
+    }
+}
+
