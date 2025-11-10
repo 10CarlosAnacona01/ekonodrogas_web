2104d6926ee3429f398542183775d20434eca443
Carlos Anacona <estebananacona13@gmail.com>
2025-11-09 20:44:21 -0500

    Adelantos del backend

--- EKONODROGAS_BACKEND/ekonodrogas/src/main/java/com/ekonodrogas/ekonodrogas/repository/VentasRepository.java
+++ EKONODROGAS_BACKEND/ekonodrogas/src/main/java/com/ekonodrogas/ekonodrogas/repository/VentasRepository.java
@@ -3,7 +3,6 @@ package com.ekonodrogas.ekonodrogas.repository;
 import com.ekonodrogas.ekonodrogas.persistence.VentasEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
